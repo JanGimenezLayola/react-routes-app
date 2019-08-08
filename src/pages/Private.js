63b3bcd26e23cbe2b3415651b327a82574e71bb5
JanGimenezLayola <gilajanvdm@mailfence.com>
2019-08-08 10:47:58 +0200

    add redirect privatepage -> homepage if user is not logged in

--- src/pages/Private.js
+++ src/pages/Private.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import {Redirect} from 'react-router-dom'
 
 function Private() {
+  const isLoggedIn = false;
   return (
     <div>
-      <h1>Private page</h1>
+      {isLoggedIn ? <h1>Private page</h1> : <Redirect to='/'/>}
     </div>
   )
 }
