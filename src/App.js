920124623b37b32d3a6651737335f8dfe0145635
JanGimenezLayola <gilajanvdm@mailfence.com>
2019-08-08 10:07:21 +0200

    import Router and add routes to pages

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React from 'react';
+
 import Home from './pages/Home';
 import About from './pages/About';
 import Private from './pages/Private';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <Home />
-      <About />
-      <Private />
-    </div>
+    return ( 
+    <Router>
+      <div className="App">
+        <Route path='/' component={Home}/>
+        <Route path='/about' component={About}/>
+        <Route path='/private' component={Private}/>
+      </div>
+    </Router>
   );
 }
 
