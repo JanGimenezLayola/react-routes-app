e139ae0aec18b17febf5b0e3b57da698e8c5997b
JanGimenezLayola <gilajanvdm@mailfence.com>
2019-08-08 10:17:15 +0200

    add nav component

---
+++ src/components/Nav.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function Nav() {
+  return (
+    <div>
+      <ul>
+        <li>
+          <a href="/">Home</a>
+        </li>
+        <li>
+          <a href="/about">About</a>
+        </li>
+        <li>
+          <a href="/private">Private</a>
+        </li>
+      </ul>
+    </div>
+  )
+}
+
+export default Nav;
