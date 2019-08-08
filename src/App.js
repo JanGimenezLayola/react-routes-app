fb49298a85b0a08d4f7e45a8bc97dda2122b45ad
JanGimenezLayola <gilajanvdm@mailfence.com>
2019-08-08 11:30:59 +0200

    add NotFound page ans add route

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import About from './pages/About';
 import Private from './pages/Private';
 import Nav from './components/Nav';
 import Details from './pages/Details';
+import NotFound from './pages/NotFound';
 
 import './App.css';
 
@@ -16,9 +17,10 @@ function App() {
         <Nav />
         <Switch>
           <Route path='/' exact component={Home}/>
-          <Route path='/about' component={About}/>
+          <Route path='/about' exact component={About}/>
           <Route path='/private' exact component={Private}/>
-          <Route path='/private/search' component={Details}/>
+          <Route path='/private/search' exact component={Details}/>
+          <Route component={NotFound}/>
         </Switch>
       </div>
     </Router>
