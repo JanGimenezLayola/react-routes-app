f5cbacf330e631c520511a42a0b919443a588120
JanGimenezLayola <gilajanvdm@mailfence.com>
2019-08-08 10:10:37 +0200

    add switch to render only one page

--- src/App.js
+++ src/App.js
@@ -3,16 +3,18 @@ import React from 'react';
 import Home from './pages/Home';
 import About from './pages/About';
 import Private from './pages/Private';
-import {BrowserRouter as Router, Route} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import './App.css';
 
 function App() {
     return ( 
     <Router>
       <div className="App">
-        <Route path='/' component={Home}/>
-        <Route path='/about' component={About}/>
-        <Route path='/private' component={Private}/>
+        <Switch>
+          <Route path='/' component={Home}/>
+          <Route path='/about' component={About}/>
+          <Route path='/private' component={Private}/>
+        </Switch>
       </div>
     </Router>
   );
